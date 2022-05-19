e87df24f01613ab9c0094680b1ab1f7b9e96a170
77 <1825642753@qq.com>
2022-05-19 18:47:45 +0800

    fix:登录页面搭建

---
+++ src/views/LoginView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="about">
+    <h1>login</h1>
+    <el-form ref="form" :model="loginModel" label-width="80px">
+      <el-form-item label="Username">
+        <el-input
+          v-model="loginModel.username"
+          placeholder="Username"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="Password">
+        <el-input
+          v-model="loginModel.password"
+          type="password"
+          placeholder="Password"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">Login</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { ElMessage } from "element-plus";
+
+const loginModel = ref({
+  username: "",
+  password: "",
+});
+const user = ref();
+const login = () => {
+  user.value = { id: 1, username: loginModel.value.username };
+  ElMessage.success("Login success");
+};
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
